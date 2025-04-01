aa8d13bf6ee8b6339cb6da55f8729276a7899df2
Ralph M. <mccloskey.ralph35@gmail.com>
2025-04-01 08:06:26 -0400

    edited create tables file and added parts to parts list

---
+++ insertData.sql
@@ -0,0 +1,40 @@
+USE repairShop;
+INSERT INTO parts
+(
+ partNum, partName, partDescription, partManufacture, partCategory, partUnit, partCost, partWarranty
+)
+VALUES
+    (
+     '113645', 'Rain-X -25', 'Windshield Washer Fluid', 'Rain-X', 'Windshield', 'Gallon', 6.29, 'None'),
+    ('54070', 'Brake Rotor', 'Front Brake Rotor', 'Duralast', 'Brakes', 'Each', 81.49, '2 Year'),
+    ('C614', 'Brake Caliper', 'Front Brake Caliper (Left)', 'Duralast', 'Brakes', 'Each', 61.99, 'Limited Lifetime'),
+    ('C615', 'Brake Caliper', 'Front Brake Caliper (Right)', 'Duralast', 'Brakes', 'Each', 61.99, 'Limited Lifetime'),
+    ('H7-AGM', 'Battery', 'Car Battery Group 7', 'Duralast', 'Electrical', 'Each', 229.99, '3 Year'),
+    ('DL1701-16-3', 'Alternator', 'Alternator', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-4', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-5', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-6', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-7', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-8', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-9', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-10','Starter','Starter','Duralast','Electrical','Each' ,181.99,'Limited Lifetime' ),
+    ('DL1701-16-11','Starter','Starter','Duralast','Electrical','Each' ,181.99,'Limited Lifetime' ),
+    ('DL1701-16-12','Starter','Starter','Duralast','Electrical','Each' ,181.99,'Limited Lifetime' ),
+    ('DL1701-16-13','Starter','Starter','Duralast','Electrical','Each' ,181.99,'Limited Lifetime' ),
+    ('DL1701-16-14','Starter','Starter','Duralast','Electrical','Each' ,181.99,'Limited Lifetime' ),
+    ('DL1701-16-15','Starter','Starter','Duralast','Electrical','Each' ,181.99,'Limited Lifetime' ),
+    ('DL1701-16-16','Starter','Starter','Duralast','Electrical','Each' ,181.99,'Limited Lifetime' ),
+    ('DL1701-16-17','Starter','Starter','Duralast','Electrical','Each' ,181.99,'Limited Lifetime' ),
+    ('DL1701-16-18','Starter','Starter','Duralast','Electrical','Each' ,181.99,'Limited Lifetime' ),
+    ('DL1701-16-19','Starter','Starter','Duralast','Electrical','Each' ,181.99,'Limited Lifetime' ),
+    ('DL1701-16-20','Starter','Starter','Duralast','Electrical','Each' ,181.99,'Limited Lifetime' ),
+    ('DL1701-16-21','Starter','Starter','Duralast','Electrical','Each' ,181.99,'Limited Lifetime' ),
+    ('DL1701-16-22', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-23', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-24', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-25', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-26', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-27', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-28', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-29', 'Starter', 'Starter', 'Duralast', 'Electrical', 'Each', 181.99, 'Limited Lifetime'),
+    ('DL1701-16-30','Starter' ,'Starter' ,'Duralast' ,'Electrical' ,'Each', 181.99, 'Limited Lifetime');
