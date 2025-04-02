0a9b0a7bcbb865687a07e880ffc516c8eb016072
Ralph McCloskey <mccloskey.ralph35@gmail.com>
2025-04-02 11:46:39 -0400

    added statements and edited data

---
+++ insertData/insertEmployees.sql
@@ -0,0 +1,14 @@
+USE repairShop;
+
+INSERT INTO employees
+(empFname, empLName, empPosition, location_id_FK)
+VALUES
+('Joe', 'Brown', 'Manager', 1),
+('Sam', 'Smucker', 'Head Mechanic', 1),
+('Henry', 'Smulligan', 'Mechanic', 1),
+('George', 'Liebig', 'Mechanic', 1),
+('Corey', 'Wiggins', 'Mechanic', 1),
+('Charlie', 'Reading', 'Manager', 2),
+('Marc', 'Blundell', 'Head Mechanic', 2),
+('Mark', 'Thomas', 'Mechanic', 2),
+('Joe', 'Smith', 'Mechanic', 2);
---
+++ insertData/insertVendors.sql
@@ -0,0 +1,7 @@
+USE repairShop;
+
+INSERT INTO vendor
+(vendorName, vendorContact, vendorAddress, vendorCity, vendorState, vendorZip, vendorPhone)
+VALUES
+('Autozone', '', '2101 Oregon Avenue', 'Philadelphia', 'PA', '19145', '21543063+6'),
+('Pep Boys', '', '2298 West Ritner Street', 'Philadelphia', 'PA', '19145', '2153367180');
